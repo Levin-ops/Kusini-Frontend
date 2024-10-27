fa98e0d305c4b6b5040a5caf5c1b4a1ba954fae1
Levin Mwanganyi Mwambi <144274243+Levin-ops@users.noreply.github.com>
2024-10-27 03:55:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Kusini-Frontend/",
   plugins: [react()],
   build: {
     outDir: "docs",
